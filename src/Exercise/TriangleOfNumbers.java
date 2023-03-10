b019b06c6863457b8a9c851cc443ed7c9aeb4474
parahod4e <k.i.peychev84@gmail.com>
2023-03-10 22:05:31 +0200

    Initial commit

---
+++ src/Exercise/TriangleOfNumbers.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //int n = 5;
+        //    //Loop to iterate over each row
+        //    for (int i = 1; i <= n; i++) {
+        //      //Loop to iterate over each column of the ith row
+        //      for (int j = 1; j <= i; j++) {
+        //        System.out.print(i + " ");
+        //      }
+        //      System.out.println();
+
+
+        int rowNumber = Integer.parseInt(scanner.nextLine());
+
+        }
+    }
+
